dd86848c800e9472f7bdf3d19b49db7deed6f2da
Burak <mburak.demir.059@gmail.com>
2022-07-14 00:06:24 +0300

    Left Side Login Page

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage:{
+        'twitter':"url('https://abs.twimg.com/sticky/illustrations/lohp_1302x955.png')",
+      }
+    },
+  },
+  plugins: [],
+}
